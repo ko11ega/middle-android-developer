49e6bb2711f1f1d32668113e773f41c9a0c244e4
kollega <ko11ega@bk.ru>
2020-06-12 15:23:42 +0300

    +item_comment.xml(primitive handmade) and edit CommentsAdapter.kt from video

--- app/src/main/java/ru/skillbranch/skillarticles/ui/article/CommentsAdapter.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/article/CommentsAdapter.kt
@@ -1,28 +1,45 @@
 package ru.skillbranch.skillarticles.ui.article
-
-import android.view.View
-import android.view.ViewGroup
+import android.view.LayoutInflater
+import android.view.View import android.view.ViewGroup
 import androidx.paging.PagedListAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.item_comment.*
+import ru.skillbranch.skillarticles.R
 import ru.skillbranch.skillarticles.data.models.CommentItemData
 import ru.skillbranch.skillarticles.ui.custom.CommentItemView
 
 class CommentsAdapter(private val listener: (CommentItemData)-> Unit) :
     PagedListAdapter<CommentItemData, CommentVH>(CommentDiffCallback()) {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CommentVH = CommentVH(CommentItemView(parent.context), listener)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CommentVH {
+        val containerView = LayoutInflater.from(parent.context).inflate(
+            R.layout.item_comment,
+            parent,
+            false
+        )
+
+        return CommentVH(containerView, listener) //TODO video 1:17:18
+    }
 
     override fun onBindViewHolder(holder: CommentVH, position: Int) {
         holder.bind(getItem(position))
     }
 }
 
+
 class CommentVH(override val containerView: View, val listener:(CommentItemData) -> Unit) :
     RecyclerView.ViewHolder(containerView), LayoutContainer {
     fun bind(item: CommentItemData?) {
-        (containerView as CommentItemView).bind(item)
-        if (item != null) itemView.setOnClickListener {listener(item)}
+
+        //TODO bind data -> ViewHolder (containerView as CommentItemView).bind(item)
+        if (item != null) {
+            itemView.setOnClickListener {listener(item)}
+            tv_author_name.text = item.user.name
+        } else {
+            //if item null show placeholder
+            tv_author_name.text = "Loading - need placeholder this"
+        }
     }
 }
 
