246d663731661ead27d9b511b88a00a61c4d3875
kollega <ko11ega@bk.ru>
2020-07-16 18:49:40 +0300

    add some files,  1:20:11

---
+++ app/src/main/java/ru/skillbranch/skillarticles/data/local/dao/ArticlesDao.kt
@@ -0,0 +1,23 @@
+package ru.skillbranch.skillarticles.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Query
+import ru.skillbranch.skillarticles.data.local.entities.Article
+
+@Dao
+interface ArticlesDao : BaseDao<Article>{
+
+    //TODO upsert
+
+    @Query("""
+        SELECT * FROM articles
+    """)
+    fun findArticles(): List<Article>
+
+    @Query("""
+        SELECT * FROM articles
+        WHERE id = :id
+    """)
+    fun findArticleById(id: String): Article
+
+}
