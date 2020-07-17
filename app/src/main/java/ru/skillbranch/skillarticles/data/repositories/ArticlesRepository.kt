270bfdaad752d087e93908461d1f1fd66d224395
kollega <ko11ega@bk.ru>
2020-07-17 19:24:43 +0300

    add some files ArticlesRepository,  1:54:00

--- app/src/main/java/ru/skillbranch/skillarticles/data/repositories/ArticlesRepository.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/data/repositories/ArticlesRepository.kt
@@ -1,10 +1,11 @@
 package ru.skillbranch.skillarticles.data.repositories
-
+import androidx.annotation.VisibleForTesting
 import androidx.lifecycle.LiveData
 import androidx.paging.DataSource
 import androidx.sqlite.db.SimpleSQLiteQuery
 import ru.skillbranch.skillarticles.data.NetworkDataHolder
 import ru.skillbranch.skillarticles.data.local.DBManager.db
+import ru.skillbranch.skillarticles.data.local.dao.*
 import ru.skillbranch.skillarticles.data.local.entities.ArticleItem
 import ru.skillbranch.skillarticles.data.local.entities.ArticleTagXRef
 import ru.skillbranch.skillarticles.data.local.entities.CategoryData
