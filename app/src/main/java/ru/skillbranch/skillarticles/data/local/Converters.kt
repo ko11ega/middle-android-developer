368ebca2d5058430db5c5648887c848f4472730e
kollega <ko11ega@bk.ru>
2020-07-18 21:09:00 +0300

    Converters.kt 2:44:00

--- app/src/main/java/ru/skillbranch/skillarticles/data/local/Converters.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/data/local/Converters.kt
@@ -1,6 +1,8 @@
 package ru.skillbranch.skillarticles.data.local
 
 import androidx.room.TypeConverter
+import ru.skillbranch.skillarticles.data.repositories.MarkdownElement
+import ru.skillbranch.skillarticles.data.repositories.MarkdownParser
 import java.util.*
 
 class DateConverter{
@@ -9,6 +11,9 @@ class DateConverter{
 
     @TypeConverter
     fun dateTotimestamp(date: Date): Long = date.time
+}
 
-
+class MarkdownConverter{
+    @TypeConverter
+    fun toMarkdown(content:String?): List<MarkdownElement>? = content?.let { MarkdownParser.parse(it) }
 }
